55e34772011d89d4c9a4ea542d413ca976e84efb
IsraShargawi <israshargawi@gmail.com>
2026-03-28 18:35:30 +0400

    payment and journals test

--- ETicketingSystem.UnitTest/UnitTest1.cs
+++ ETicketingSystem.UnitTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using ETicketingSystem.Accounting.Entities;
+using ETicketingSystem.Accounting.Services;
 using ETicketingSystem.Common;
 using ETicketingSystem.Data;
 using ETicketingSystem.Payment.Handlers;
@@ -27,7 +29,106 @@ namespace ETicketingSystem.UnitTest
         //Todo
         //Test concurrecy case
         //Test double entyr bookkeeping
+        [Fact]
+        public async Task RecordPayment_ShouldCreateDoubleEntry()
+        {
+            var context = GetDbContext();
+            var ledgerService = new LedgerService(context);
+
+            // Setup chart of accounts
+            var cashCOA = new ChartOfAccount
+            {
+                Code = "1100",
+                Name = "Cash",
+                Category = AccountCategory.Asset,
+                CreatedAt = DateTime.UtcNow,
+                CreatedBy = 1
+            };
+
+            var revenueCOA = new ChartOfAccount
+            {
+                Code = "4100",
+                Name = "Revenue",
+                Category = AccountCategory.Revenue,
+                CreatedAt = DateTime.UtcNow,
+                CreatedBy = 1
+            };
+
+            context.ChartOfAccounts.AddRange(cashCOA, revenueCOA);
+            await context.SaveChangesAsync();
+
+            var cashAccount = new Account
+            {
+                ChartOfAccountId = cashCOA.Id,
+                Code = "1100",
+                Name = "Cash Account",
+                Balance = 0,
+                CreatedAt = DateTime.UtcNow,
+                CreatedBy = 1
+            };
+
+            var revenueAccount = new Account
+            {
+                ChartOfAccountId = revenueCOA.Id,
+                Code = "4100",
+                Name = "Sales Revenue",
+                Balance = 0,
+                CreatedAt = DateTime.UtcNow,
+                CreatedBy = 1
+            };
+
+            context.Accounts.AddRange(cashAccount, revenueAccount);
+            await context.SaveChangesAsync();
+
+            await ledgerService.RecordPaymentLedgerAsync(1, 100m, PaymentMethod.CreditCard);
+
+            var entries = await context.JournalEntries.ToListAsync();
+            Assert.Equal(2, entries.Count);
+
+            var debitEntry = entries.First(e => e.Debit > 0);
+            var creditEntry = entries.First(e => e.Credit > 0);
+
+            Assert.Equal(100m, debitEntry.Debit);
+            Assert.Equal(100m, creditEntry.Credit);
+        }
+
         //Test jounal entry balance check
+        [Fact]
+        public async Task ValidateLedgerBalance_ShouldBeBalanced()
+        {
+            var context = GetDbContext();
+            var ledgerService = new LedgerService(context);
+
+            context.JournalEntries.AddRange(
+                new JournalEntry
+                {
+                    SourceId = 1,
+                    SourceType = "Payment",
+                    AccountId = 1,
+                    Debit = 100,
+                    Credit = 0,
+                    CreatedAt = DateTime.UtcNow,
+                    CreatedBy = 1
+                },
+                new JournalEntry
+                {
+                    SourceId = 1,
+                    SourceType = "Payment",
+                    AccountId = 2,
+                    Debit = 0,
+                    Credit = 100,
+                    CreatedAt = DateTime.UtcNow,
+                    CreatedBy = 1
+                }
+            );
+
+            await context.SaveChangesAsync();
+
+            var isBalanced = await ledgerService.ValidateLedgerBalanceAsync();
+
+            Assert.True(isBalanced);
+        }
+
         //QRCode delay test
         [Fact]
         public async Task QRScan_ShouldTakeAtLeast8Seconds()
