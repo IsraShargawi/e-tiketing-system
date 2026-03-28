bcc06ab1a981b443f76e5871e9f017d3fe1194d4
IsraShargawi <israshargawi@gmail.com>
2026-03-28 17:48:52 +0400

    get db context

--- ETicketingSystem.UnitTest/UnitTest1.cs
+++ ETicketingSystem.UnitTest/UnitTest1.cs
@@ -1,7 +1,20 @@
+using ETicketingSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ETicketingSystem.UnitTest
 {
     public class UnitTest1
     {
+        private ApplicationDbContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new ApplicationDbContext(options);
+            return context;
+        }
+
         [Fact]
         public void Test1()
         {
